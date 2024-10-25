01e04c57dec1ff4e8162bf6a0af8b17b06eafe5f
chutao <chutao.zhang@outlook.com>
2024-10-25 20:31:55 +0800

    去掉打印信息

--- src/services/userService.js
+++ src/services/userService.js
@@ -218,7 +218,7 @@ class UserService {
             }
 
             const resCount = updateUserFlag ? 1 + (roleIds?.length || 0) : 1;
-console.log('tempCount', tempCount, 'resCount', resCount, roleIds);
+            
             if (tempCount === resCount) {
                 if (logContent.length > 0) {
                     logContent.push(`账号：${user?.account}`);
